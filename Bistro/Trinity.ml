6b9b122226a24e9f8d643d63158109a3590fa6cb
Carine <carine.rey@ens-lyon.fr>
2016-03-24 19:36:57 +0100

    try to resolve read_normalization bug

--- Bistro/Trinity.ml
+++ Bistro/Trinity.ml
@@ -6,7 +6,7 @@ open Bistro_bioinfo.Std
 let trinity (fastq: _ fastq workflow) : fasta workflow =
 	workflow [
 		mkdir_p dest;
-		cmd "Trinity" [ string "-single" ; dep fastq ;
+		cmd "Trinity" [ opt "-single" dep fastq;
 						string "--seqType fq --max_memory 1G --full_cleanup " ;
 						string "--output" ; seq ~sep:"/" [ dest ; string "trinity"] ] ;
 	]
@@ -19,13 +19,13 @@ let fastq2fasta (fastq : _ fastq workflow ) : fasta workflow =
     
  
 
-let read_normalization seq_type memmory max_cov nb_cpu (fastq: _ fastq workflow) :  _ fastq workflow =
+let read_normalization seq_type memmory max_cov nb_cpu (fastq: _ fastq workflow)  : _ fastq workflow =
 	Bistro.Workflow.make [%sh{|
 	TRINITY_PATH=`which Trinity`
 	TRINTIY_DIR_PATH=`dirname $TRINITY_PATH`
 	READ_NORMALISATION_PATH=$TRINTIY_DIR_PATH/util/insilico_read_normalization.pl
-	$READ_NORMALISATION_PATH --seqType {{ seqtype }} --JM {{memmory}}G --max_cov {{max_cov}} --CPU {{nb_cpu}} --output {{ dest }} --single {{ fastq }}
-	|} ]
+	$READ_NORMALISATION_PATH --seqType {{ seq_type }} --JM {{memmory}}G --max_cov {{max_cov}} --CPU {{nb_cpu}} --output {{ dest }} --single {{ fastq }}
+	|}]
 	
 let fastool seq_type memmory max_cov nb_cpu (fastq: _ fastq workflow) :  _ fastq workflow =
 	Bistro.Workflow.make [%sh {|
