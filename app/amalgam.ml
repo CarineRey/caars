312df664a7608bfc00411cb41cd07eff6376d9da
Carine <carine.rey@ens-lyon.fr>
2016-08-01 17:32:40 +0200

    adapt to apytram change output prefix, add ref name

--- app/amalgam.ml
+++ app/amalgam.ml
@@ -250,7 +250,7 @@ let trinity_annotated_fams_of_trinity_assemblies configuration_dir   =
 let parse_apytram_results apytram_annotated_ref_fams =
   let config = Bistro.Expr.(
       List.map apytram_annotated_ref_fams ~f:(fun (s, f, w) ->
-        seq ~sep:"\t" [ string s.species ; string f ; dep w ]
+        seq ~sep:"\t" [ string s.species ; string s.ref_species ; string f ; dep w ]
       )
       |> seq ~sep:"\n"
     )
