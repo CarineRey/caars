1ffdd5f3d3953d97c17018e9f0485b7c55f4bd30
Carine <carine.rey@ens-lyon.fr>
2016-08-01 15:44:03 +0000

    add only_best_file option in lib/apytram

--- lib/apytram.ml
+++ lib/apytram.ml
@@ -92,6 +92,7 @@ let apytram
         option (flag string "--plot") plot ;
         option (flag string "--plot_ali") plot_ali ;
         option (flag string "--no_best_file") no_best_file ;
+	option (flag string "--only_best_file") only_best_file ;
         (*option (opt "-memory" ident) memory ;*)
         (*opt "-memory" int 2 ;*)
         opt "-memory" ident (seq [ string "$((" ; mem ; string " / 1024))" ]) ;
