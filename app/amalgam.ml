6847ca24b36e5d6dc5287acfb5b2af6dd21a193a
Carine <carine.rey@ens-lyon.fr>
2016-08-29 16:20:56 +0200

    replace config file by sample sheet file

--- app/amalgam.ml
+++ app/amalgam.ml
@@ -84,7 +84,7 @@ let parse_orientation id = function
   | "FR" -> Some (Right FR)
   | "UP" -> Some (Right UP)
   | "-"  -> None
-  | _    -> failwith ({| Syntax error in configuration file sample -> "|} ^ id ^ {|": orientation must be in ["F","R","RF","FR","US","UP"] |})
+  | _    -> failwith ({|Syntax error in the sample sheet file (sample -> "|} ^ id ^ {|": orientation must be in ["F","R","RF","FR","US","UP"] |})
 
 let parse_line_fields_of_rna_conf_file = function
   | [ id ; species ; ref_species ; path_fastq_single ; path_fastq_left ; path_fastq_right ; orientation ; run_trinity ; path_assembly ; run_apytram] ->
@@ -92,17 +92,17 @@ let parse_line_fields_of_rna_conf_file = function
      let run_trinity = match run_trinity with
        | "yes" | "Yes" | "y" | "Y" -> true
        | "no" | "No" | "n" | "N" -> false
-       | _ -> failwith ({| Syntax error in configuration file sample -> "|} ^ id ^ {|": run_trinity must be "yes" or "no" |})
+       | _ -> failwith ({| Syntax error inthe sample sheet file (sample -> "|} ^ id ^ {|"): run_trinity must be "yes" or "no" |})
      in
      let (path_assembly,given_assembly) = match (path_assembly,run_trinity) with
        | ("-" ,_) -> ("-",false)
        | (path,true) -> (path,true)
-       | (path,false) -> failwith ({| Syntax error in configuration file sample -> "|} ^ id ^ {|": you gave a trinity assembly path but run_trinity is false. It is incompatible.|})
+       | (path,false) -> failwith ({|Syntax error in the sample sheet file (sample -> "|} ^ id ^ {|": you gave a trinity assembly path but run_trinity is false. It is incompatible.|})
      in
      let run_apytram = match run_apytram with
        | "yes" | "Yes" | "y" | "Y" -> true
        | "no" | "No" | "n" | "N" -> false
-       | _ -> failwith ({| Syntax error in configuration file sample -> "|} ^ id ^ {|": run_apytram must be "yes" or "no" |})
+       | _ -> failwith ({|Syntax error in the sample sheet file (sample -> "|} ^ id ^ {|": run_apytram must be "yes" or "no" |})
      in
 
      let sample_fastq = match (parse_fastq_path path_fastq_single,
@@ -114,11 +114,11 @@ let parse_line_fields_of_rna_conf_file = function
        | ( None    , Some  _, Some _, Some (Right o), _    , _    ) -> Fastq_Paired_end (path_fastq_left, path_fastq_right, o)
        | ( Some  _ , None   , None  , Some (Left o) , _    , _    ) -> Fastq_Single_end (path_fastq_single, o)
        | ( None    , None   , None  , None          , false, true ) -> Fastq_Single_end ("-", US)
-       | ( None    , None   , None  , _             , true , _    ) -> failwith ({| Syntax error in configuration file sample -> "|} ^ id ^ {|": You didn't give any RNA-seq data, but you ask to run apytram : it is impossible, apytram needs raw RNA-seq data.)|})
-       | ( Some  _ , None   , None  , Some (Right o), _    , _    ) -> failwith ({| Syntax error in configuration file sample -> "|} ^ id ^ {|": Incompatible choice. Path for a single-end data but an orientation for a paired-end data.|})
-       | ( None    , Some  _, Some _, Some (Left o) , _    , _    ) -> failwith ({| Syntax error in configuration file sample -> "|} ^ id ^ {|": Incompatible choice. Paths for paired-end data but an orientation for a single-end data.|})
-       | ( _       , _      , _     , None          , _    , _    ) -> failwith ({| Syntax error in configuration file sample -> "|} ^ id ^ {|": No given orientation.|})
-       | _ -> failwith ({| Syntax error in configuration file sample -> "|} ^ id ^ {|": Incompatible choices. path_fastq_single must be "-" if data are "paired-end" and path_fastq_left and path_fastq_right must be "-" if data are "single-end".|})(*(path_fastq_single ^ path_fastq_left ^ path_fastq_right ^ orientation)*)
+       | ( None    , None   , None  , _             , true , _    ) -> failwith ({|Syntax error in the sample sheet file (sample -> "|} ^ id ^ {|"): You didn't give any RNA-seq data, but you ask to run apytram : it is impossible, apytram needs raw RNA-seq data.|})
+       | ( Some  _ , None   , None  , Some (Right o), _    , _    ) -> failwith ({|Syntax error in the sample sheet file (sample -> "|} ^ id ^ {|"): Incompatible choice. Path for a single-end data but an orientation for a paired-end data.|})
+       | ( None    , Some  _, Some _, Some (Left o) , _    , _    ) -> failwith ({|Syntax error in the sample sheet file (sample -> "|} ^ id ^ {|"): Incompatible choice. Paths for paired-end data but an orientation for a single-end data.|})
+       | ( _       , _      , _     , None          , _    , _    ) -> failwith ({|Syntax error in the sample sheet file (sample -> "|} ^ id ^ {|"): No given orientation.|})
+       | _ -> failwith ({|Syntax error in the sample sheet file (sample -> "|} ^ id ^ {|"): Incompatible choices. path_fastq_single must be "-" if data are "paired-end" and path_fastq_left and path_fastq_right must be "-" if data are "single-end".|})(*(path_fastq_single ^ path_fastq_left ^ path_fastq_right ^ orientation)*)
      in
      { id ;
        species ;
@@ -130,7 +130,7 @@ let parse_line_fields_of_rna_conf_file = function
        given_assembly ;
        run_apytram
      }
-  | _ -> failwith "Syntax error in configuration file"
+  | _ -> failwith "Syntax error in the sample sheet file. There aren't 10 tab delimited columns."
 
 let parse_rna_conf_file path =
   In_channel.read_lines path
@@ -585,7 +585,7 @@ let main config_file outdir species_tree_file alignments_dir seq2sp_dir np memor
 let spec =
   let open Command.Spec in
   empty
-  +> flag "--config"          (required file)   ~doc:"PATH Configuration file."
+  +> flag "--sample_sheet"    (required file)   ~doc:"PATH sample sheet file."
   +> flag "--outdir"          (required string) ~doc:"PATH Destination directory."
   +> flag "--species-tree"    (required file)   ~doc:"ABSOLUTE PATH Species tree file in nw format containing all species. Warning absolute path is required."
   +> flag "--alignment-dir"   (required string) ~doc:"PATH Directory containing all gene family alignments (Family_name.fa) in fasta format."
