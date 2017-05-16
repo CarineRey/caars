3486643ca055f48f2f0541375f8350e9ba60f70a
Carine <carine.rey@ens-lyon.fr>
2017-05-16 17:27:37 +0200

    add amalgam_user to sudoer

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -13,11 +13,14 @@ if [ -n "$LOCAL_USER_ID" ]
 then
 USER_ID=${LOCAL_USER_ID:-9001}
 echo "Starting with UID : $USER_ID"
+echo "Add sudo right (in the docker container)"
+apt-get clean && apt-get update && apt-get install --no-install-recommends -qy sudo
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user_amalgam
+adduser user_amalgam sudo
+echo "user_amalgam ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 export HOME=/home/user_amalgam
 exec /usr/local/bin/gosu user_amalgam "$@"
 else
-export HOME=/home/user
 echo "Starting with UID : root"
 exec "$@"
 fi
