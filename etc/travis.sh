f3d69b85adc06cabbfcee9e2e64971d1f68526f3
Carine <carine.rey@ens-lyon.fr>
2017-06-22 17:48:38 +0200

    test travis

--- etc/travis.sh
+++ etc/travis.sh
@@ -1,10 +1,13 @@
 sudo apt-get update -qq
-sudo apt-get install -y opam
+sudo apt-get install -y opam git
 
 opam init --comp="$OCAML_VERSION"
 eval $(opam config env)
 
+opam install -y core=v0.9.1
 opam install -y bistro
 
+git clone https://github.com/CarineRey/caars.git
+
 make
-make test
+#make test
