94f507f374a0637e81b95fdcf93be1d64e89b9b6
Philippe Veber <philippe.veber@gmail.com>
2017-06-23 09:47:10 +0200

    travis: not necessary to clone caars repo

--- etc/travis.sh
+++ etc/travis.sh
@@ -7,7 +7,7 @@ eval $(opam config env)
 opam install -y core=v0.9.1
 opam install -y bistro
 
-git clone https://github.com/CarineRey/caars.git
+#git clone https://github.com/CarineRey/caars.git
 
 make
-#make test
+make test
