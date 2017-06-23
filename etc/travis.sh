702510edfd0f7ffe64aae296543b4ae52ea4f207
Philippe Veber <philippe.veber@gmail.com>
2017-06-23 10:00:57 +0200

    travis: say yes to opam init

--- etc/travis.sh
+++ etc/travis.sh
@@ -2,10 +2,10 @@ sudo add-apt-repository --yes ppa:avsm/ocaml42+opam12
 sudo apt-get update -qq
 sudo apt-get install -y opam git
 
-opam init --comp="$OCAML_VERSION"
+opam init -y --comp="$OCAML_VERSION"
 eval $(opam config env)
 
-opam install -y core=v0.9.1
+opam install -y #core=v0.9.1
 opam install -y bistro
 
 #git clone https://github.com/CarineRey/caars.git
