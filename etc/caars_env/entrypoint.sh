b8d549c0717004a1204f1da7eaf1998b6a3fdda4
Carine <carine.rey@ens-lyon.fr>
2017-11-14 20:38:28 +0100

    update env docker

--- etc/caars_env/entrypoint.sh
+++ etc/caars_env/entrypoint.sh
@@ -4,6 +4,12 @@
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
 
+
+USER_ID=${LOCAL_USER_ID:-9001}
+Xvfb :1 -screen 0 1024x768x16 &
+export DISPLAY=:1
+
+
 if [ -n "$SHARED_DIR" ]
 then
 W_DIR=$SHARED_DIR
@@ -19,11 +25,11 @@ then
 USER_ID=${LOCAL_USER_ID:-9001}
 echo "Starting with UID : $USER_ID"
 echo "To be root, type \" sudo su - \""
-useradd --shell /bin/bash -u $USER_ID -o -c "" -g sudo -m user_caars
-echo "user_caars ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-export HOME=/home/user_caars
+useradd --shell /bin/bash -u $USER_ID -o -c "" -g sudo -m user_docker
+echo "user_docker ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+export HOME=/home/user_docker
 cd $HOME
-exec /usr/local/bin/gosu user_caars "$@"
+exec /usr/local/bin/gosu user_docker "$@"
 else
 echo "Starting with UID : root"
 exec "$@"
