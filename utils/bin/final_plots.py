a488aa71ea7da8cb4329a07865b864934319deab
Carine <carine.rey@ens-lyon.fr>
2017-11-29 10:17:23 +0100

    check if files exist

--- utils/bin/final_plots.py
+++ utils/bin/final_plots.py
@@ -2,25 +2,25 @@
 # coding: utf-8
 
 #  final_plots.py
-#  
+#
 #  Copyright 2017 Carine Rey <carine.rey@ens-lyon.fr>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 import os
 import glob
@@ -105,7 +105,6 @@ print "target_species_l = %s" %(target_species_l)
 
 df_seq2sp = pd.read_table(all_fam_seq2sp_fn, names=["seq", "sp", "fam", "subfam"])
 
-df_seq2sp["fam_subfam"] = df_seq2sp["fam"] + "_" + df_seq2sp["subfam"]
 
 #### Fig param
 plt.style.use('seaborn-white')
@@ -124,6 +123,7 @@ plt.rcParams['figure.autolayout'] = True
 
 
 
+
 #### Fig 1
 plt.figure();
 df_sp = df_seq2sp.groupby(['sp']).size().reset_index(name='counts')
@@ -137,110 +137,112 @@ fig1 = df_sp.plot(kind='barh', x="sp", y="counts", title= "# of total sequences"
 fig1_plot = fig1.get_figure()
 fig1_plot.savefig("%s/total_seqs.pdf" %(OutDirName))
 
+if os.path.isfile(all_fam_orthologs_fn):
 
-#### Fig 2
-
-plt.figure();
-df_subfam = df_seq2sp.groupby(['fam_subfam']).size().reset_index(name='counts')
-df_subfam.to_csv("%s/total_sp_per_subfam.tsv" %(OutDirName), index = False)
-
-fig2 = df_subfam.plot.hist(by='counts', bins=50, legend=False, color="b")
-fig2.set_xlabel("# of sequences per subfamily")
-fig2_plot = fig2.get_figure()
-fig2_plot.savefig("%s/total_sp_per_subfam.pdf" %(OutDirName))
+    df_seq2sp["fam_subfam"] = df_seq2sp["fam"] + "_" + df_seq2sp["subfam"]
 
 
-df_subfam = df_seq2sp.groupby(['fam_subfam', "sp"]).size().reset_index(name='counts')
-df_subfam.to_csv("%s/total_sp_per_subfam_sp.tsv" %(OutDirName), index = False)
+    #### Fig 2
+
+    plt.figure();
+    df_subfam = df_seq2sp.groupby(['fam_subfam']).size().reset_index(name='counts')
+    df_subfam.to_csv("%s/total_sp_per_subfam.tsv" %(OutDirName), index = False)
+
+    fig2 = df_subfam.plot.hist(by='counts', bins=50, legend=False, color="b")
+    fig2.set_xlabel("# of sequences per subfamily")
+    fig2_plot = fig2.get_figure()
+    fig2_plot.savefig("%s/total_sp_per_subfam.pdf" %(OutDirName))
 
 
-
-#### Fig 3
-plt.figure();
-df_subfam_per_sp = pd.crosstab(index=df_subfam['fam_subfam'], columns=[df_subfam['sp']])
-nb_sp = df_subfam_per_sp.shape[1]
-df_subfam_per_sp["sum"] = df_subfam_per_sp.sum(1)
-df_subfam_per_sp["%_sum"] = df_subfam_per_sp["sum"] * 100. / float(nb_sp)
-df_subfam_per_sp.to_csv("%s/nb_seq_per_sp_per_subfam.tsv" %(OutDirName))
-
-fig3 = df_subfam_per_sp["%_sum"].plot.hist(bins=50, legend=False, color="b", range=[0,100])
-fig3.set_xlabel("% of the whole number of species per subfamily")
-fig3_plot = fig3.get_figure()
-fig3_plot.savefig("%s/presence_sp_per_subfam.pdf" %(OutDirName))
+    df_subfam = df_seq2sp.groupby(['fam_subfam', "sp"]).size().reset_index(name='counts')
+    df_subfam.to_csv("%s/total_sp_per_subfam_sp.tsv" %(OutDirName), index = False)
 
 
 
+    #### Fig 3
+    plt.figure();
+    df_subfam_per_sp = pd.crosstab(index=df_subfam['fam_subfam'], columns=[df_subfam['sp']])
+    nb_sp = df_subfam_per_sp.shape[1]
+    df_subfam_per_sp["sum"] = df_subfam_per_sp.sum(1)
+    df_subfam_per_sp["%_sum"] = df_subfam_per_sp["sum"] * 100. / float(nb_sp)
+    df_subfam_per_sp.to_csv("%s/nb_seq_per_sp_per_subfam.tsv" %(OutDirName))
 
-#### Fig 4
-
- 
-summary_files =  glob.glob(os.path.join(InputDirName_filter, "FilterSummary_out/*.filter_summary.txt"))
-
-print summary_files
-print InputDirName_filter
-list_df = []
-for f in summary_files:
-    if os.path.exists(f) and os.path.getsize(f) > 1:
-        list_df.append(pd.read_table(f, names=["seq", "n_seq", "%_id", "%_ali", "t", "s"]))
-        print 
-
-df_filter_summary = pd.concat(list_df)
-
-def get_id(seq):
-    seq = seq.split("_")[0]
-    sp_id = ''.join([x for x in seq if x.isalpha()][2:])
-    return target_species_d[sp_id]
-
-
-df_filter_summary["sp"] = df_filter_summary["seq"].map(get_id)
-df_filter_summary["color"] =  df_filter_summary["s"].replace({"K":"#09BB21", "K2":"#FFA500", "D":"#D80002"})
-
-print df_filter_summary
-print df_filter_summary.shape
+    fig3 = df_subfam_per_sp["%_sum"].plot.hist(bins=50, legend=False, color="b", range=[0,100])
+    fig3.set_xlabel("% of the whole number of species per subfamily")
+    fig3_plot = fig3.get_figure()
+    fig3_plot.savefig("%s/presence_sp_per_subfam.pdf" %(OutDirName))
 
 
 
-for t_sp in target_species_l:
-    print t_sp
-    df_filter_summary_sp = df_filter_summary.copy()
-    df_filter_summary_sp = df_filter_summary_sp[df_filter_summary.sp.eq(t_sp)]
-    
-    #df_filter_summary_sp_melt = pd.melt(df_filter_summary_sp[["seq", "%_id", "%_ali", "s"]], id_vars=['seq', "s"], value_vars=["%_id", "%_ali"])
-    #print df_filter_summary_sp_melt
-    #p = ggplot(aes(x="value", fill = "s"), data=df_filter_summary_sp_melt) + theme_bw() +\
-    #    geom_histogram(bins = 100, color="black") + facet_grid("variable ~ .", scales="free") + \
-    #    ggtitle(t_sp)
-    #ggsave(plot = p, filename = OutDirName + "/" + t_sp+'.pdf')
-    
-    
-    plt.figure(1)
-    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(4,4))
-    print "K"
-    print df_filter_summary_sp["%_id"][df_filter_summary_sp["s"].isin(["K","K2"])]
-    print "D"
-    print df_filter_summary_sp["%_id"][df_filter_summary_sp["s"].eq("D")]
-    
-    print " "
-    ax1.hist([df_filter_summary_sp["%_id"][df_filter_summary_sp["s"].isin(["K","K2"])].values,
-             df_filter_summary_sp["%_id"][df_filter_summary_sp["s"].eq("D")].values],
-             stacked=True, color=["#008000", "#C41C00"], bins=50, label=["K", "D"], range=[0,100])
-    
-    handles, labels = ax1.get_legend_handles_labels()
-    ax1.legend(handles, labels)
+if os.path.isdir(os.path.join(InputDirName_filter, "FilterSummary_out/")):
 
-    ax2.hist([df_filter_summary_sp["%_ali"][df_filter_summary_sp["s"].isin(["K","K2"])].values,
-             df_filter_summary_sp["%_ali"][df_filter_summary_sp["s"].eq("D")].values],
-             stacked=True, color=["#008000", "#C41C00"], bins=50, label=["K", "D"], range=[0,100])
-    
-    handles, labels = ax1.get_legend_handles_labels()
-    ax1.legend(handles, labels)
-    ax1.set_xlabel("%_id")
-    ax1.set_title(t_sp)
-    
-    ax2.set_xlabel("%_ali")
-    handles2, labels2 = ax2.get_legend_handles_labels()
-    ax2.legend(handles2, labels2)
-    plt.tight_layout()
-    plt.savefig(OutDirName + "/" + t_sp+'.filter_stats.pdf')
+    #### Fig 4
+    summary_files =  glob.glob(os.path.join(InputDirName_filter, "FilterSummary_out/*.filter_summary.txt"))
+
+    print summary_files
+    print InputDirName_filter
+    list_df = []
+    for f in summary_files:
+        if os.path.exists(f) and os.path.getsize(f) > 1:
+            list_df.append(pd.read_table(f, names=["seq", "n_seq", "%_id", "%_ali", "t", "s"]))
+
+    df_filter_summary = pd.concat(list_df)
+
+    def get_id(seq):
+        seq = seq.split("_")[0]
+        sp_id = ''.join([x for x in seq if x.isalpha()][2:])
+        return target_species_d[sp_id]
+
+
+    df_filter_summary["sp"] = df_filter_summary["seq"].map(get_id)
+    df_filter_summary["color"] =  df_filter_summary["s"].replace({"K":"#09BB21", "K2":"#FFA500", "D":"#D80002"})
+
+    print df_filter_summary
+    print df_filter_summary.shape
+
+
+
+    for t_sp in target_species_l:
+        print t_sp
+        df_filter_summary_sp = df_filter_summary.copy()
+        df_filter_summary_sp = df_filter_summary_sp[df_filter_summary.sp.eq(t_sp)]
+
+        #df_filter_summary_sp_melt = pd.melt(df_filter_summary_sp[["seq", "%_id", "%_ali", "s"]], id_vars=['seq', "s"], value_vars=["%_id", "%_ali"])
+        #print df_filter_summary_sp_melt
+        #p = ggplot(aes(x="value", fill = "s"), data=df_filter_summary_sp_melt) + theme_bw() +\
+        #    geom_histogram(bins = 100, color="black") + facet_grid("variable ~ .", scales="free") + \
+        #    ggtitle(t_sp)
+        #ggsave(plot = p, filename = OutDirName + "/" + t_sp+'.pdf')
+
+
+        plt.figure(1)
+        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(4,4))
+        print "K"
+        print df_filter_summary_sp["%_id"][df_filter_summary_sp["s"].isin(["K","K2"])]
+        print "D"
+        print df_filter_summary_sp["%_id"][df_filter_summary_sp["s"].eq("D")]
+
+        print " "
+        ax1.hist([df_filter_summary_sp["%_id"][df_filter_summary_sp["s"].isin(["K","K2"])].values,
+                 df_filter_summary_sp["%_id"][df_filter_summary_sp["s"].eq("D")].values],
+                 stacked=True, color=["#008000", "#C41C00"], bins=50, label=["K", "D"], range=[0,100])
+
+        handles, labels = ax1.get_legend_handles_labels()
+        ax1.legend(handles, labels)
+
+        ax2.hist([df_filter_summary_sp["%_ali"][df_filter_summary_sp["s"].isin(["K","K2"])].values,
+                 df_filter_summary_sp["%_ali"][df_filter_summary_sp["s"].eq("D")].values],
+                 stacked=True, color=["#008000", "#C41C00"], bins=50, label=["K", "D"], range=[0,100])
+
+        handles, labels = ax1.get_legend_handles_labels()
+        ax1.legend(handles, labels)
+        ax1.set_xlabel("%_id")
+        ax1.set_title(t_sp)
+
+        ax2.set_xlabel("%_ali")
+        handles2, labels2 = ax2.get_legend_handles_labels()
+        ax2.legend(handles2, labels2)
+        plt.tight_layout()
+        plt.savefig(OutDirName + "/" + t_sp+'.filter_stats.pdf')
 
 
