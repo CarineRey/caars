4409c9cb45ec02f6b3fe13d5d3ea4f7e7981a763
Carine <carine.rey@ens-lyon.fr>
2017-12-14 19:12:37 +0100

    update

--- etc/build_caars_env.sh
+++ etc/build_caars_env.sh
@@ -4,7 +4,7 @@ set -eo
 
 IMAGE_NAME=caars_env
 DOCKERFILE_DIR=caars_env
-TAG="0.2.0-bistrodev"
+TAG="0.2.0-bistro3"
 REPO=carinerey/$IMAGE_NAME:$TAG
 cp -r ../utils $DOCKERFILE_DIR
 docker build -t $REPO $DOCKERFILE_DIR
