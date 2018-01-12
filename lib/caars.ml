700f68d4ff108436e64ac8af9b938e64b27c21e1
Carine <carine.rey@ens-lyon.fr>
2018-01-12 14:34:56 +0100

    update ExtractOrthologs.py version

--- lib/caars.ml
+++ lib/caars.ml
@@ -623,7 +623,7 @@ let write_orthologs_relationships (merged_and_reconciled_families_dirs:'a workfl
                    Some(List.map configuration.all_ref_samples ~f:(fun s -> s.species)))
         | false -> (None, None)
     in
-    workflow ~descr:"ExtractOrthologs.py" ~version:6 [
+    workflow ~descr:"ExtractOrthologs.py" ~version:7 [
             mkdir_p dest;
             cmd "ExtractOrthologs.py" ~env [
             ident dest;
