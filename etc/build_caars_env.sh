208bbb51e797df651b1d890b1b12532a80bbeaa6
Carine <carine.rey@ens-lyon.fr>
2018-01-23 19:35:30 +0100

    use dev tagged docker

--- etc/build_caars_env.sh
+++ etc/build_caars_env.sh
@@ -4,7 +4,7 @@ set -euo pipefail +o nounset
 
 IMAGE_NAME=caars_env
 DOCKERFILE_DIR=caars_env
-TAG="master"
+TAG="0.2.0-bistrodev"
 REPO=carinerey/$IMAGE_NAME:$TAG
 cp -r ../utils $DOCKERFILE_DIR
 docker build -t $REPO $DOCKERFILE_DIR
