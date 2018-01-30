1d9957bba26ccf9f102ca79a76b062a218d57ad6
Carine <carine.rey@ens-lyon.fr>
2018-01-30 10:18:16 +0100

    update caars docker building

--- etc/build_caars.sh
+++ etc/build_caars.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-set -eo
+set -euo pipefail +o nounset
 
 ### CAARS
 IMAGE_NAME=caars
 DOCKERFILE_DIR=caars
-REPO=carinerey/$IMAGE_NAME
-docker build --no-cache -t $REPO $DOCKERFILE_DIR
-docker push carinerey/$IMAGE_NAME
+
+export HASH=`git rev-parse --short HEAD`
+TAG=$HASH
+REPO=carinerey/$IMAGE_NAME:$TAG
+
+docker build --no-cache --build-arg BRANCH_DEV=$BRANCH -t $REPO $DOCKERFILE_DIR
+push_flag=$1
+
+if [[ $push_flag == "push_yes" ]]
+then
+    docker push $REPO
+fi
