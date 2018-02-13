1409958a28e72f3cc50662a307714ed79d3fbea5
Carine <carine.rey@ens-lyon.fr>
2018-02-13 16:43:08 +0100

    fix error, select only species of the same group in apytram

--- lib/caars.ml
+++ lib/caars.ml
@@ -889,7 +889,7 @@ let build_term configuration =
                         let guide_query = concat ~descr (List.map ref_species ~f:(fun sp -> configuration_dir / ref_fams sp fam.name)) in
                         let target_query = build_target_query ref_species fam.name configuration trinity_annotated_fams apytram_group in
                         let query = concat ~descr:(descr ^ ".+seqdispatcher") [guide_query; target_query] in
-                        let compressed_reads_dbs = List.filter_map reads_blast_dbs ~f:(fun (s, db) -> if s.ref_species = ref_species then Some db else None) in
+                        let compressed_reads_dbs = List.filter_map reads_blast_dbs ~f:(fun (s, db) -> if (s.ref_species = ref_species && s.apytram_group = apytram_group)then Some db else None) in
                         let time_max = 18000 * List.length compressed_reads_dbs in
                         let w = Apytram.apytram_multi_species ~descr ~time_max ~no_best_file:true ~write_even_empty:true ~plot:false ~i:5 ~evalue:1e-10 ~out_by_species:true ~memory:divided_thread_memory ~fam:fam.name ~query compressed_reads_dbs in
                         List.filter_map configuration.apytram_samples ~f:(fun s ->
