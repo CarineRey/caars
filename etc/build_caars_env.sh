0e4d9af16ed0675b81d7bb8072aa586e3104f75b
Carine Rey <carine.rey@ens-lyon.fr>
2020-03-30 11:58:12 +0200

    update docker_env

--- etc/build_caars_env.sh
+++ etc/build_caars_env.sh
@@ -4,7 +4,7 @@ set -euo pipefail +o nounset
 
 IMAGE_NAME=caars_env
 DOCKERFILE_DIR=caars_env
-TAG="master"
+TAG="generax_20200330"
 REPO=carinerey/$IMAGE_NAME:$TAG
 cp -r ../utils $DOCKERFILE_DIR
 docker build -t $REPO $DOCKERFILE_DIR
