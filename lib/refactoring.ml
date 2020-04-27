083db80a711a77551e84af7dab88d1a763c2996a
Philippe Veber <philippe.veber@gmail.com>
2020-04-27 11:30:07 +0200

    refactoring: added fasta_reads step

--- lib/refactoring.ml
+++ lib/refactoring.ml
@@ -5,6 +5,11 @@ type 'a assoc = (string, 'a) List.Assoc.t
 
 let ( $ ) a k = List.Assoc.find_exn ~equal:String.equal a k
 
+let assoc_opt keys ~f =
+  List.filter_map keys ~f:(fun k ->
+      Option.map (f k) ~f:(fun v -> k, v)
+    )
+
 class type blast_db = object
   inherit text
   method format : [`blast_db]
@@ -31,6 +36,9 @@ module SE_or_PE_file = struct
         reads2 : 'a file ;
         orientation : paired_end_orientation ;
       }
+
+  let se reads orientation = Single_end { reads ; orientation }
+  let pe reads1 reads2 orientation = Paired_end { reads1 ; reads2 ; orientation }
 end
 
 module Sample_file = struct
@@ -42,15 +50,14 @@ end
 module Rna_sample = struct
   type t = {
     id : string ;
+    group_id : string ;
     species : string ;
-    apytram_group : string ;
-    ref_species : string list;
+    reference_species : string list ;
     sample_file : Sample_file.t ;
     run_trinity : bool ;
     run_transdecoder : bool ;
-    path_assembly : string ;
-    given_assembly : bool ;
     run_apytram : bool ;
+    precomputed_assembly : string option ;
   }
 end
 
@@ -60,16 +67,8 @@ module Configuration = struct
     | Length
     | Length_complete
 
-  type item = {
-    id : string ;
-    group_id : string ;
-    species : string ;
-    reference_species : string ;
-    sample : Rna_sample.t ;
-  }
-
   type t = {
-    samples : item list ;
+    samples : Rna_sample.t list ;
     reference_species : string list ;
     reference_transcriptome : fasta file assoc ;
   }
@@ -79,7 +78,7 @@ module Configuration = struct
   let make ~reference_transcriptome ~samples = {
     samples ;
     reference_species = (
-      List.map samples ~f:(fun (it : item) -> it.reference_species)
+      List.concat_map samples ~f:(fun (it : Rna_sample.t) -> it.reference_species)
       |> List.dedup_and_sort ~compare:String.compare
     ) ;
     reference_transcriptome ;
@@ -88,8 +87,37 @@ end
 
 module Pipeline = struct
   type t = {
+    fasta_reads : (Rna_sample.t, fasta SE_or_PE_file.t) List.Assoc.t ;
     ref_blast_dbs : blast_db file assoc ;
   }
+
+  let rna_sample_needs_rna (s : Rna_sample.t) =
+    match s.run_apytram, s.run_trinity, s.precomputed_assembly with
+    | true, _, _          -> true
+    | false, true, Some _ -> false
+    | false, true, None   -> true
+    | false, false, _     -> false
+
+  let fasta_reads (config : Configuration.t) =
+    assoc_opt config.samples ~f:(fun s ->
+        if rna_sample_needs_rna s then
+          let open SE_or_PE_file in
+          let fq2fa ?label x =
+            let label = Option.value_map label ~default:"" ~f:(( ^ ) "_") in
+            let descr = sprintf "%s_%s%s" s.id s.species label in
+            Trinity.fastq2fasta ~descr x
+          in
+          match s.sample_file with
+          | Fasta_sample_file fa -> Some fa
+          | Fastq_sample_file (Single_end se) ->
+            Some (SE_or_PE_file.se (fq2fa se.reads) se.orientation)
+          | Fastq_sample_file (Paired_end pe) ->
+            Some (SE_or_PE_file.pe
+                    (fq2fa ~label:"left" pe.reads1)
+                    (fq2fa ~label:"right" pe.reads2)
+                    pe.orientation)
+        else None
+      )
 
   let ref_blast_dbs (config : Configuration.t) =
     List.map config.reference_species ~f:(fun ref_species ->
@@ -101,5 +129,6 @@ module Pipeline = struct
 
   let make configuration =
     let ref_blast_dbs = ref_blast_dbs configuration in
-    { ref_blast_dbs }
+    let fasta_reads = fasta_reads configuration in
+    { ref_blast_dbs ; fasta_reads }
 end
