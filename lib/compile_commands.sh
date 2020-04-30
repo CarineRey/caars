b6d8c9bfa69a80f565f8f4cc2e927cee6b14a804
Philippe Veber <philippe.veber@gmail.com>
2020-04-30 22:42:58 +0200

    deleted deprecated scripts

--- lib/compile_commands.sh
+++
@@ -1,1 +0,0 @@
-ocamlbuild -use-ocamlfind -tag thread -pkgs bistro.utils,bistro.bioinfo,ppx_bistro Main.byte
--- lib/test_compile.sh
+++
@@ -1,1 +0,0 @@
-ocamlbuild -use-ocamlfind -tag thread -pkgs bistro.utils,bistro.bioinfo,ppx_bistro Test_bistro.byte && ./Test_bistro.byte test.fasta test_out
